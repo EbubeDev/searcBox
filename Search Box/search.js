4b9465c57bdb6be3103b0029bed7afc18e93f827
EbubeDev <75450150+EbubeDev@users.noreply.github.com>
2021-07-02 18:38:30 +0100

    Add files via upload

---
+++ Search Box/search.js
@@ -0,0 +1,7 @@
+
+var searchBox = document.getElementById("searchBox");
+var googleIcon = document.getElementById("icon");
+
+googleIcon.onclick = function(){
+  searchBox.classList.toggle("active");
+}
